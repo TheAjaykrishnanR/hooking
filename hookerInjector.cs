4b2cd7ea9b5857bd4d34a486a4260df5c91788e4
AjaykrishnanR <theconsciousstoictaoistcat@gmail.com>
2025-07-21 04:58:09 +0530

    -

--- hookerInjector.cs
+++ hookerInjector.cs
@@ -1,13 +1,45 @@
 using System;
+using System.Runtime.InteropServices;
 
 public class HookerInjector
 {
-	public static void Main()
+	[DllImport("kernel32.dll", SetLastError = true)]
+	public static extern nint OpenProcess(uint dwDesiredAccess, bool bInheritHandles, uint dwProcessId);
+	[DllImport("kernel32.dll", SetLastError = true)]
+	public static extern int CloseHandle(nint handle);
+
+	public static unsafe void Main()
 	{
-		Hooker.Hook();
+		Console.Write("processId: ");
+		uint processId = Convert.ToUInt32(Console.ReadLine());
+		const uint PROCESS_ALL_ACCESS = 0x1FFFFF;
+		nint hProcess = OpenProcess(PROCESS_ALL_ACCESS, false, processId);
+		Console.WriteLine($"hProcess: {hProcess}");
+		GetModulesInProcess(hProcess);
+		CloseHandle(hProcess);
 	}
 
-	public void Inject()
+	[DllImport("psapi.dll", SetLastError = true)]
+	public static extern int EnumProcessModules(nint hProcess, [Out] nint ptrToModuleArray, int moduleArrayLength, [Out] nint sizeNeeded);
+
+	public static unsafe void GetModulesInProcess(nint hProcess)
+	{
+		nint[] modules = new nint[100];
+		uint sizeNeeded = 0;
+		fixed (nint ptr = (nint)modules)
+		{
+			if (EnumProcessModules(hProcess, ptr, modules.Length * sizeof(nint), (nint)(&sizeNeeded)) == 0)
+			{
+				Console.WriteLine($"EnumProcessModules() failed, win32: {Marshal.GetLastWin32Error()}");
+				return;
+			}
+		}
+
+		Console.WriteLine("processes...");
+		foreach (nint module in modules) Console.WriteLine($"m: {module}");
+	}
+
+	public static void Inject()
 	{
 		// 1. write hooker.dll to target process's memory 
 		// 2. write "hooker.dll" string to target process memory
